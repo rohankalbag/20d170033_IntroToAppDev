519cd9d85a6658697a64781fbee96fb2be18e160
Rohan Rajesh Kalbag <46604893+rohankalbag@users.noreply.github.com>
2021-07-28 13:32:59 +0530

    Hide Password

--- Week 4/lib/screens/signup.dart
+++ Week 4/lib/screens/signup.dart
@@ -13,24 +13,26 @@ class EmailSignUp extends StatefulWidget {
 class _EmailSignUpState extends State<EmailSignUp> {
   final _formKey = GlobalKey<FormState>();
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-  DatabaseReference dbRef = FirebaseDatabase.instance.reference().child("Users");
+  DatabaseReference dbRef =
+      FirebaseDatabase.instance.reference().child("Users");
   TextEditingController emailController = TextEditingController();
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
   void registerToFb() async {
-    UserCredential userData = await firebaseAuth.createUserWithEmailAndPassword(email: emailController.text, password: passwordController.text);
+    UserCredential userData = await firebaseAuth.createUserWithEmailAndPassword(
+        email: emailController.text, password: passwordController.text);
     String uid = userData.user.uid;
     dbRef.child(uid).set({
-        "uid": uid,
-        "email": emailController.text,
-        "name": nameController.text,
-      }).then((res) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => Home(uid: uid)),
-        );
-      }).catchError((err) {
+      "uid": uid,
+      "email": emailController.text,
+      "name": nameController.text,
+    }).then((res) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Home(uid: uid)),
+      );
+    }).catchError((err) {
       showDialog(
           context: context,
           builder: (BuildContext context) {
@@ -47,7 +49,7 @@ class _EmailSignUpState extends State<EmailSignUp> {
               ],
             );
           });
-          });
+    });
   }
 
   @override
@@ -58,114 +60,107 @@ class _EmailSignUpState extends State<EmailSignUp> {
           title: Text("Authentication"),
           backgroundColor: Colors.purple,
         ),
-      body: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.fromLTRB(10,150,10,10),
-                  child: TextFormField(
-                    style: TextStyle(color: Colors.grey[300]),
-                    controller: nameController,
-                    decoration: InputDecoration(
-                      labelStyle: TextStyle(color: Colors.grey[300]),
-                      labelText: "Enter Username",
-                      enabledBorder: OutlineInputBorder(
+        body: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+                child: Column(children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 150, 10, 10),
+                child: TextFormField(
+                  style: TextStyle(color: Colors.grey[300]),
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    labelStyle: TextStyle(color: Colors.grey[300]),
+                    labelText: "Enter Username",
+                    enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10.0),
-                          borderSide: BorderSide(
-                            color: Colors.grey[300],
-                            width: 2.0,
-                          )
-                      ),
-                    ),
-                    // The validator receives the text that the user has entered.
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Enter Username';
-                      }
-                      return null;
-                    },
+                        borderSide: BorderSide(
+                          color: Colors.grey[300],
+                          width: 2.0,
+                        )),
                   ),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Enter Username';
+                    }
+                    return null;
+                  },
                 ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(10,10,10,10),
-                    child: TextFormField(
-                      style: TextStyle(color: Colors.grey[300]),
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        labelStyle: TextStyle(color: Colors.grey[300]),
-                        labelText: "Enter Email",
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                            borderSide: BorderSide(
-                              color: Colors.grey[300],
-                              width: 2.0,
-                            )
-                        ),
-                      ),
-                      // The validator receives the text that the user has entered.
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return 'Enter Email Address';
-                        } else if (!value.contains('@')) {
-                          return 'Please enter a valid email address!';
-                        }
-                        return null;
-                      },
-                    ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: TextFormField(
+                  style: TextStyle(color: Colors.grey[300]),
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelStyle: TextStyle(color: Colors.grey[300]),
+                    labelText: "Enter Email",
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          color: Colors.grey[300],
+                          width: 2.0,
+                        )),
                   ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(10,10,10,10),
-                    child: TextFormField(
-                      style: TextStyle(color: Colors.grey[300]),
-                      controller: passwordController,
-                      decoration: InputDecoration(
-                        labelStyle: TextStyle(color: Colors.grey[300]),
-                        labelText: "Enter Password",
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                            borderSide: BorderSide(
-                              color: Colors.grey[300],
-                              width: 2.0,
-                            )
-                        ),
-                      ),
-                      // The validator receives the text that the user has entered.
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return 'Enter Password';
-                        } else if (value.length < 6) {
-                          return 'Password must be at least 6 characters!';
-                        }
-                        return null;
-                      },
-                    ),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Enter Email Address';
+                    } else if (!value.contains('@')) {
+                      return 'Please enter a valid email address!';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: TextFormField(
+                  obscureText: true,
+                  style: TextStyle(color: Colors.grey[300]),
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    labelStyle: TextStyle(color: Colors.grey[300]),
+                    labelText: "Enter Password",
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide(
+                          color: Colors.grey[300],
+                          width: 2.0,
+                        )),
                   ),
-                  SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          if (_formKey.currentState.validate()) {
-                            registerToFb();
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.purple,
-                            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                            textStyle: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold)),
-                        child: Text('Submit'),
-                      ),
-                    ],
-                  )
-              ]
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Enter Password';
+                    } else if (value.length < 6) {
+                      return 'Password must be at least 6 characters!';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        registerToFb();
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.purple,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                        textStyle: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold)),
+                    child: Text('Submit'),
+                  ),
+                ],
               )
-          )
-      )
-    );
+            ]))));
   }
 }
